fd3542b048a51ecc4f15360be27ca2eaabf0fd18
almemanuel <emanuel.almeida1998@outlook.com>
2022-10-19 13:18:42 -0300

    #REFACT adição das type annotations

--- scripts/divide_csv.py
+++ scripts/divide_csv.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv('src/db/base.csv', sep=';')
-df_states = df[(~df['estado'].isna()) & (~df['codmun'].isna())]
-df_brasil = df[df.regiao == 'Brasil']
+df:pd.DataFrame = pd.read_csv('src/db/base.csv', sep=';')
+df_states:pd.DataFrame = df[(~df['estado'].isna()) & (~df['codmun'].isna())]
+df_brasil:pd.DataFrame = df[df.regiao == 'Brasil']
+
 df_states.to_csv('src/db/states.csv')
 df_brasil.to_csv('src/db/brasil.csv')
